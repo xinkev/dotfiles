11e57e17c8c7d869ccef28d3883609d7fbae9c6a
xinkev <lyx13710@mail.com>
2025-07-07 13:55:35 +0900

    add more alias

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -2,8 +2,12 @@
 alias ls="ls --color=auto"
 alias ll='exa --long --all --group-directories-first --git'
 alias wl='wl-copy'
-alias gcm='git commit'
-alias gcam='git commit --amend'
+alias gaa='git add .'
+alias gcm='git commit -m'
+alias gcam='git commit --amend -m'
 alias gco='git checkout'
+alias glog='git log'
 alias gr='git reset'
 alias grh='git reset --hard'
+alias gs='git status'
+alias gus="git restore --staged"
